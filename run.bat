df59814fd3bfb4281b52233b558e4622559c6fb8
fastict <fast@ict.usc.edu>
2017-10-04 11:26:29 -0700

    stanford-parser now runs properly with github folder layout

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 
-pushd StanfordParserWrapper\bin\Release 
-start StanfordParserWrapper.exe %1 %2 %3 %4 %5 %6 %7 %8
+pushd StanfordParserWrapper
+start bin\Release\StanfordParserWrapper.exe %1 %2 %3 %4 %5 %6 %7 %8
 popd
